3cddab7ab0752bf05dd34839287efe786d7d631f
Nikita <73557906+nekitoxic@users.noreply.github.com>
2021-01-13 16:08:17 +1000

    add bundle

--- src/Controller/ConferenceController.php
+++ src/Controller/ConferenceController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -11,8 +12,12 @@ class ConferenceController extends AbstractController
     /**
      * @Route("/", name="home")
      */
-    public function index(): Response
+    public function index(Request $request): Response
     {
+        $greet = '';
+            if ($name = $request->query->get('hello')) {
+                $greet = sprintf('<h1>Hello %s!</h1>', htmlspecialchars($name));
+            }
         return $this->render('conference/index.html.twig', [
             'controller_name' => 'ConferenceController',
         ]);
