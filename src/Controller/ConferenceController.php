2d1ef240a62f91c8dfe86779d282e935cd03f0db
Nikita <73557906+nekitoxic@users.noreply.github.com>
2021-01-13 16:31:45 +1000

    Update ConferenceController.php

--- src/Controller/ConferenceController.php
+++ src/Controller/ConferenceController.php
@@ -10,7 +10,7 @@ use Symfony\Component\Routing\Annotation\Route;
 class ConferenceController extends AbstractController
 {
     /**
-     * @Route("/", name="home")
+     * @Route("/", name="homepage")
      */
     public function index(Request $request): Response
     {
